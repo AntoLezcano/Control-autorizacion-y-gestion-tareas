afe1f6036d2ad1d185afb5baec5b6126393f65f7
Antonella <antonella.beti@gmail.com>
2024-09-16 12:04:05 -0300

    se creo la conexion a la base de datos y se puso en eescucha el servidor

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import morgan from "morgan";
+import cookie from "cookie-parser";
+import { router } from "./routes/User.Routes.js";
+import { routerTarea } from "./routes/Task.routes.js";
+import cors from "cors";
+
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookie());
+app.use(routerTarea);
+app.use(router);
+
+export { app };
